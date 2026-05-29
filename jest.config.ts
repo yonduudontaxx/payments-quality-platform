8cf8625648657062e8b9680b95417e109c8d0e8e
joshuahub88 <joshua.maigeri@yolo.com>
2026-05-29 10:41:17 +0300

    fix: downgrade Jest to v29 to match ts-jest v29, simplify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,21 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>/test'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: false,
+      tsconfig: {
+        module: 'CommonJS',
+        moduleResolution: 'node',
+      },
+    }],
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
-  },
 };
+
+export default config;
